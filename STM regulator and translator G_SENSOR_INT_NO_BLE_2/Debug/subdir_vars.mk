79504c6a2471ade2d063911726c1f967c2045a48
candy6886 <ww115@ic.ac.uk>
2017-09-08 02:58:59 +0800

    Add files via upload

---
+++ STM regulator and translator G_SENSOR_INT_NO_BLE_2/Debug/subdir_vars.mk
@@ -0,0 +1,70 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CFG_SRCS += \
+../main.cfg 
+
+CMD_SRCS += \
+../CC2650_LAUNCHXL.cmd 
+
+C_SRCS += \
+../CC2650_LAUNCHXL.c \
+../GSENSOR_SYSTEM_main_STM.c \
+../LIS2DS12.c \
+../ccfg.c 
+
+GEN_CMDS += \
+./configPkg/linker.cmd 
+
+GEN_FILES += \
+./configPkg/linker.cmd \
+./configPkg/compiler.opt 
+
+GEN_MISC_DIRS += \
+./configPkg/ 
+
+C_DEPS += \
+./CC2650_LAUNCHXL.d \
+./GSENSOR_SYSTEM_main_STM.d \
+./LIS2DS12.d \
+./ccfg.d 
+
+GEN_OPTS += \
+./configPkg/compiler.opt 
+
+OBJS += \
+./CC2650_LAUNCHXL.obj \
+./GSENSOR_SYSTEM_main_STM.obj \
+./LIS2DS12.obj \
+./ccfg.obj 
+
+GEN_MISC_DIRS__QUOTED += \
+"configPkg\" 
+
+OBJS__QUOTED += \
+"CC2650_LAUNCHXL.obj" \
+"GSENSOR_SYSTEM_main_STM.obj" \
+"LIS2DS12.obj" \
+"ccfg.obj" 
+
+C_DEPS__QUOTED += \
+"CC2650_LAUNCHXL.d" \
+"GSENSOR_SYSTEM_main_STM.d" \
+"LIS2DS12.d" \
+"ccfg.d" 
+
+GEN_FILES__QUOTED += \
+"configPkg\linker.cmd" \
+"configPkg\compiler.opt" 
+
+C_SRCS__QUOTED += \
+"../CC2650_LAUNCHXL.c" \
+"../GSENSOR_SYSTEM_main_STM.c" \
+"../LIS2DS12.c" \
+"../ccfg.c" 
+
+
