4762b125e2f8cc39c0d082da4d70b1fd7080e624
candy6886 <ww115@ic.ac.uk>
2017-09-08 02:45:51 +0800

    Add files via upload

---
+++ polling method without BLE/polling method without BLE/Debug/subdir_vars.mk
@@ -0,0 +1,70 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CFG_SRCS += \
+../empty.cfg 
+
+CMD_SRCS += \
+../CC2650_LAUNCHXL.cmd 
+
+C_SRCS += \
+../CC2650_LAUNCHXL.c \
+../LIS2DS12.c \
+../ccfg.c \
+../polling\ method\ main.c 
+
+GEN_CMDS += \
+./configPkg/linker.cmd 
+
+GEN_FILES += \
+./configPkg/linker.cmd \
+./configPkg/compiler.opt 
+
+GEN_MISC_DIRS += \
+./configPkg/ 
+
+C_DEPS += \
+./CC2650_LAUNCHXL.d \
+./LIS2DS12.d \
+./ccfg.d \
+./polling\ method\ main.d 
+
+GEN_OPTS += \
+./configPkg/compiler.opt 
+
+OBJS += \
+./CC2650_LAUNCHXL.obj \
+./LIS2DS12.obj \
+./ccfg.obj \
+./polling\ method\ main.obj 
+
+GEN_MISC_DIRS__QUOTED += \
+"configPkg\" 
+
+OBJS__QUOTED += \
+"CC2650_LAUNCHXL.obj" \
+"LIS2DS12.obj" \
+"ccfg.obj" \
+"polling method main.obj" 
+
+C_DEPS__QUOTED += \
+"CC2650_LAUNCHXL.d" \
+"LIS2DS12.d" \
+"ccfg.d" \
+"polling method main.d" 
+
+GEN_FILES__QUOTED += \
+"configPkg\linker.cmd" \
+"configPkg\compiler.opt" 
+
+C_SRCS__QUOTED += \
+"../CC2650_LAUNCHXL.c" \
+"../LIS2DS12.c" \
+"../ccfg.c" \
+"../polling method main.c" 
+
+
